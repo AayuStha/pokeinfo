d4e2f2dcad2f4f3e1a3bd0f13473b8ec3f125551
Aayush Shrestha <aayustha21@gmail.com>
2025-03-25 15:43:14 +0545

    commit v1.3

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -100,7 +100,7 @@ const Login: React.FC = () => {
         </form>
 
         <p className="mt-6 text-center text-sm text-indigo-200">
-          Demo credentials: admin / pokemon123
+          Demo credentials: admin / hello
         </p>
       </div>
     </div>
